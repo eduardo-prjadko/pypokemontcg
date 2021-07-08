8e76bc83bc65b405d7036a5a2052c8e6555e7b53
eduardo.prjadko <eduardoabp@gmail.com>
2021-07-08 13:07:01 -0300

    add set tests

---
+++ tests/test_sets.py
@@ -0,0 +1,19 @@
+from . import pokemon_client
+
+
+def test_cards_all(pokemon_client):
+    first_r = pokemon_client.sets.all()
+    if first_r.has_next:
+        second_r = pokemon_client.sets.next()
+        assert second_r.status_code == 200
+        assert second_r.page == 2
+    assert first_r.has_next != None
+    assert isinstance(first_r.pages, int) 
+    assert first_r.status_code == 200
+    assert first_r.page == 1
+    
+
+def test_last_has_next(pokemon_client):
+    r = pokemon_client.sets.all()
+    r = pokemon_client.sets.all(page=r.pages)
+    assert r.has_next == False
