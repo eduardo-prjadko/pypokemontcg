9bd18ed191b966008cc93d1bfcb09ed337dd0d43
eduardo.prjadko <eduardoabp@gmail.com>
2021-07-08 11:24:43 -0300

    add doc strings

--- pypokemontcg/api_collections/cards.py
+++ pypokemontcg/api_collections/cards.py
@@ -2,6 +2,9 @@ from .base_api import BaseApi
 
 
 class Cards(BaseApi):
+    """
+    Class that holds all cards endpoint APIs
+    """
 
     ENDPOINT = 'cards'
     count = None
@@ -14,12 +17,25 @@ class Cards(BaseApi):
         }
     
     def all(self, page=1, page_size=250):
+        """
+        Download all cards data, with the maximum page size of 250
+
+        :param page: page number
+        :param page_size: size of the page. Max size is 250
+
+        :return: namedtuple
+        """
         self.count = page
         self.size = page_size
         params = self._set_params(self.count, self.size)
         return self.get_result(f'{self.base_url}{self.ENDPOINT}', params=params)
 
     def next(self):
+        """
+        calls the next page based on the last method all() page 
+
+        :return: namedtuple
+        """
         self.count += 1
         params = self._set_params(self.count, self.size)
         return self.get_result(f'{self.base_url}{self.ENDPOINT}', params=params)
