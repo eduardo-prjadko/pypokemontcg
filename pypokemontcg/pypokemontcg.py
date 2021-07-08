8ef35a180441037ce6b235ac85827b81431bbe9d
eduardo.prjadko <eduardoabp@gmail.com>
2021-07-08 15:16:38 -0300

    adjust docstrings

--- pypokemontcg/pypokemontcg.py
+++ pypokemontcg/pypokemontcg.py
@@ -1,6 +1,5 @@
 """
 Code for the PokemonTCG class, that wraps the APIs into a single client object.
-Each pokemon microservice have its dedicate module inside the api_collections package
 """
 import requests
 
@@ -14,7 +13,8 @@ class PokemonTCG:
 
     def __init__(self, api_key=None, session=None):
         """
-        Setup the parameters values and create attributes composing with the api_collections modules
+        Setup the parameters values and create attributes 
+        composing with the api_collections modules
 
         :param api_key: Your Pokemon api_key
         :param session: a requests Session object, to reuse the TCP connection
