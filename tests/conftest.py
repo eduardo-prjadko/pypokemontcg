b69e8d1bad4ef88257490266c032cb6c4ee5a67e
Eduardo Prjadko <eduardoabp@gmail.com>
2022-04-21 18:47:48 -0300

    increase cards tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from pypokemontcg import PokemonTCG
 
 
 @pytest.fixture
-def pokemon_client():
+def pokemon_client() -> PokemonTCG:
     api_key = os.environ.get('API_KEY')
     client = PokemonTCG(api_key)
     return client
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,13 +1,22 @@
+import logging
+
+from pypokemontcg import PokemonTCG
 
 
-def test_cards_all(pokemon_client):
+def test_cards_all(pokemon_client: PokemonTCG):
     first_r = pokemon_client.cards.all()
+
+    logging.debug(f'Pokemon cards initiated with:')
+    logging.debug(f'- {first_r.total_count} cards to retrieve.')
+    logging.debug(f'- {first_r.pages} pages to inter.')
+
     if first_r.has_next:
         second_r = pokemon_client.cards.next()
     assert first_r.has_next != None
     assert isinstance(first_r.pages, int) 
     assert first_r.status_code == 200
     assert first_r.page == 1
+    assert first_r.page_size == 250
     assert second_r.status_code == 200
     assert second_r.page == 2
 
